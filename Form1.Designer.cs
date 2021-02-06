b36951c42cec1747743c5ff58d0a1171d00ccad5
Darren Lin <swwad1@gmail.com>
2021-02-07 00:54:25 +0800

    first upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace DogOnTop
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.watchPanel = new System.Windows.Forms.Panel();
+            this.labelWatch = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.timerWatch = new System.Windows.Forms.Timer(this.components);
+            this.iconDogOnTop = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupBox1.SuspendLayout();
+            this.watchPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.watchPanel);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(5, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(252, 136);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "拖曳視窗至此置頂";
+            // 
+            // watchPanel
+            // 
+            this.watchPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.watchPanel.Controls.Add(this.labelWatch);
+            this.watchPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.watchPanel.Location = new System.Drawing.Point(3, 23);
+            this.watchPanel.Name = "watchPanel";
+            this.watchPanel.Size = new System.Drawing.Size(246, 110);
+            this.watchPanel.TabIndex = 0;
+            // 
+            // labelWatch
+            // 
+            this.labelWatch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelWatch.Location = new System.Drawing.Point(0, 0);
+            this.labelWatch.Name = "labelWatch";
+            this.labelWatch.Size = new System.Drawing.Size(246, 110);
+            this.labelWatch.TabIndex = 0;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonStart.Location = new System.Drawing.Point(8, 147);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(120, 35);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "開始置頂";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Enabled = false;
+            this.buttonCancel.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonCancel.Location = new System.Drawing.Point(134, 147);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(120, 35);
+            this.buttonCancel.TabIndex = 2;
+            this.buttonCancel.Text = "取消置頂";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // timerWatch
+            // 
+            this.timerWatch.Interval = 250;
+            this.timerWatch.Tick += new System.EventHandler(this.timerWatch_Tick);
+            // 
+            // iconDogOnTop
+            // 
+            this.iconDogOnTop.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.iconDogOnTop.BalloonTipText = "執行中...";
+            this.iconDogOnTop.BalloonTipTitle = "DogOnTop";
+            this.iconDogOnTop.Icon = ((System.Drawing.Icon)(resources.GetObject("iconDogOnTop.Icon")));
+            this.iconDogOnTop.Text = "DogOnTop 執行中...";
+            this.iconDogOnTop.DoubleClick += new System.EventHandler(this.iconDogOnTop_DoubleClick);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 193);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Text = "Dog On Top";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mainForm_FormClosing);
+            this.Resize += new System.EventHandler(this.mainForm_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.watchPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel watchPanel;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Timer timerWatch;
+        private System.Windows.Forms.Label labelWatch;
+        private System.Windows.Forms.NotifyIcon iconDogOnTop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DogOnTop
+{
+    public partial class mainForm : Form
+    {
+        private const int HWND_TOPMOST = -1;
+        private const int HWND_NOTOPMOST = -2;
+        private const int SWP_NOMOVE = 0x0002;
+        private const int SWP_NOSIZE = 0x0001;
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, int uFlags);
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string lpWindowClass, string lpWindowName);
+
+        private static IntPtr intPtrDesktop;
+        private static bool timerLock = true;
+
+        public mainForm()
+        {
+            InitializeComponent();
+            timerWatch.Tag = null;
+            intPtrDesktop = FindWindow("Progman", "Program Manager");
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            EnableWatchTimer(true);
+            buttonStart.Enabled = false;
+            buttonCancel.Enabled = true;
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            EnableWatchTimer(false);
+            buttonStart.Enabled = true;
+            buttonCancel.Enabled = false;
+        }
+
+        private void timerWatch_Tick(object sender, EventArgs e)
+        {
+            IntPtr intPtr = GetForegroundWindow();
+            if (intPtr == Process.GetCurrentProcess().MainWindowHandle ||
+                intPtr == intPtrDesktop ||
+                timerLock)
+            {
+                return;
+            }
+            else
+            {
+                if (watchPanel.RectangleToScreen(watchPanel.ClientRectangle).Contains(MousePosition))
+                {
+                    EnableWatchTimer(false);
+                    timerWatch.Tag = intPtr;
+                    string topTitle = GetActiveWindowTitle(intPtr) ?? "";
+                    DialogResult result = MessageBox.Show(topTitle, "開始置頂", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                    if (result == DialogResult.OK && SetWindowPos(intPtr, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE))
+                    {
+                        labelWatch.Text = topTitle + " 置頂中...";
+                        ShowTrayIcon(false);
+                    }
+                }
+            }
+        }
+
+        private void EnableWatchTimer(bool enable)
+        {
+            timerLock = !enable;
+            timerWatch.Enabled = enable;
+            if (!enable && timerWatch.Tag != null)
+            {
+                SetWindowPos((IntPtr)timerWatch.Tag, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+                timerWatch.Tag = null;
+                labelWatch.Text = "";
+            }
+        }
+
+        private void ShowTrayIcon(bool enable)
+        {
+            if (enable)
+            {
+                Show();
+                this.WindowState = FormWindowState.Normal;
+                iconDogOnTop.Visible = false;
+            }
+            else
+            {
+                Hide();
+                iconDogOnTop.Visible = true;
+                iconDogOnTop.ShowBalloonTip(2000);
+            }
+        }
+
+        private string GetActiveWindowTitle(IntPtr handle)
+        {
+            const int nChars = 512;
+            StringBuilder Buff = new StringBuilder(nChars);
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+
+        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            EnableWatchTimer(false);
+        }
+
+        private void iconDogOnTop_DoubleClick(object sender, EventArgs e)
+        {
+            ShowTrayIcon(true);
+        }
+
+        private void mainForm_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                ShowTrayIcon(false);
+            }
+        }
+    }
+}
